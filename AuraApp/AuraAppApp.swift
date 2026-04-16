ea05752ffbf003a4d984d929f78d282fd27132da
Wahyu Kurniawan <ayungavis@gmail.com>
2026-04-16 15:40:43 +0800

    Initial Commit

---
+++ AuraApp/AuraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraAppApp.swift
+//  AuraApp
+//
+//  Created by Wahyu Kurniawan on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
